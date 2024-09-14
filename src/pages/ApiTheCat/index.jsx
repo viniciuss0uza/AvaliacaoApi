175b8fb618282e04a936476b1a1111a090a213d1
Vinícius <vinicius.m.sousa3009@gmail.com>
2024-09-14 13:55:08 -0300

    Apenas improviso, falta fazer essa página.

---
+++ src/pages/ApiTheCat/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export default function ApiTheCat() {
+    return (
+        //-----------Apena improviso pra pagina rodar, fazer essa página---------------
+        <div className='about-page'>
+            <h2>Venha você também descobrir o universo do Rick and Morty</h2>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur atque natus amet harum nobis excepturi rerum consequatur mollitia illum dolores, tempore ratione earum, reprehenderit alias modi inventore? Nulla, dolorem quasi?</span>
+        </div>
+    )
+}
