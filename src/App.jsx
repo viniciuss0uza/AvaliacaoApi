055525afa0f975f5d56a10220e770383e3d4a272
Vinícius <vinicius.m.sousa3009@gmail.com>
2024-09-20 18:10:05 -0300

    Ajustado

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/.about' element={<About />} />
-        <Route path='/.ApiTheCat' element={<ApiTheCat />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/ApiTheCat' element={<ApiTheCat />} />
       </Routes>
       <Footer />
     </>
