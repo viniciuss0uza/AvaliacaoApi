08b4f834e56b4e8e01cc9ba48accaac0c626bd53
Vinícius <vinicius.m.sousa3009@gmail.com>
2024-09-28 13:26:53 -0300

    Ajustado e concuído✅

--- src/pages/ApiTheCat/index.jsx
+++ src/pages/ApiTheCat/index.jsx
@@ -2,25 +2,19 @@ import { useEffect, useState } from 'react'
 import './styles.css'
 import Card from '../../components/Card'
 
-
 export default function ApiTheCat() {
     const [ conteudo, setConteudo ] = useState(<>Carregando...</>)
 
     async function getCharacters() {
-        // return fetch
         const reqOptions = {
             method: "GET",
             redirect: "follow"
         }
     
-
-    const response = await fetch(
-        <div className="card">
-            <img className='imgem' src="https://api.thecatapi.com/v1/images/0XYvRd7oD"alt="Gato" />
-            reqOptions
-        </div>
+    const response = await fetch("https://api.thecatapi.com/v1/images/0XYvRd7oD", 
+        reqOptions
     )
-
+    
     if(!response.ok){
         throw new Error("Http Error")
     }
@@ -31,12 +25,12 @@ export default function ApiTheCat() {
     }
 
     async function buildCards() {
-        const consulta = await getCharacters()
+       const consulta = await getCharacters()
         
         return (
             <>
-                { consulta.name }
-                <img src={consulta.url} alt="" />
+                {consulta.name}
+                <img src={consulta.url} alt="Gato" />
             </>
         )
     }
