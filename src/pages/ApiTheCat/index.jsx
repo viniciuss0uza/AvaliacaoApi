a9ac08c35e56004666194db02e4c2d81b99fd3aa
Vinícius <vinicius.m.sousa3009@gmail.com>
2024-09-27 21:59:43 -0300

    Falta finalizar fazendo o fetch

--- src/pages/ApiTheCat/index.jsx
+++ src/pages/ApiTheCat/index.jsx
@@ -1,62 +1,88 @@
-//-----------------Arrumar-------------------
 import { useEffect, useState } from 'react'
 import './styles.css'
-import Card from '../../components/Card'
-import Filter from '../../components/Filter'
-import Pagination from '../../components/Pagination'
+// import Card from '../../components/Card'
 
-export default function RickAndMortyApi() {
-    const [ conteudo, setConteudo ] = useState(<></>)
-    const [ busca, setBusca ] = useState('');
-    const [ page, setPage ] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-  
-    async function carregarTodosPersonagens() {
-      var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-      
-      const response = await fetch(
-        `https://api.thecatapi.com/v1/images/0XYvRd7oD`
-      )
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-  
-      const data = await response.json();
-  
-      return { info: data.info, char: data.results, }
+const mock = {
+            "id": "0XYvRd7oD",
+            "url": "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
+            "breeds": [
+              {
+                "weight": {
+                  "imperial": "7  -  10",
+                  "metric": "3 - 5"
+                },
+                "id": "abys",
+                "name": "Abyssinian",
+                "cfa_url": "http://cfa.org/Breeds/BreedsAB/Abyssinian.aspx",
+                "vetstreet_url": "http://www.vetstreet.com/cats/abyssinian",
+                "vcahospitals_url": "https://vcahospitals.com/know-your-pet/cat-breeds/abyssinian",
+                "temperament": "Active, Energetic, Independent, Intelligent, Gentle",
+                "origin": "Egypt",
+                "country_codes": "EG",
+                "country_code": "EG",
+                "description": "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+                "life_span": "14 - 15",
+                "indoor": 0,
+                "lap": 1,
+                "alt_names": "",
+                "adaptability": 5,
+                "affection_level": 5,
+                "child_friendly": 3,
+                "dog_friendly": 4,
+                "energy_level": 5,
+                "grooming": 1,
+                "health_issues": 2,
+                "intelligence": 5,
+                "shedding_level": 2,
+                "social_needs": 5,
+                "stranger_friendly": 5,
+                "vocalisation": 1,
+                "experimental": 0,
+                "hairless": 0,
+                "natural": 1,
+                "rare": 0,
+                "rex": 0,
+                "suppressed_tail": 0,
+                "short_legs": 0,
+                "wikipedia_url": "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
+                "hypoallergenic": 0,
+                "reference_image_id": "0XYvRd7oD"
+              }
+            ],
+            "width": 1204,
+            "height": 1445
+}
+
+export default function ApiTheCat() {
+    const [ conteudo, setConteudo ] = useState(<>Carregando...</>)
+
+    async function getCharacters() {
+        return mock
     }
-  
-    async function listaPersonagens() {
-      const { char: todosPersonagens, info } = await carregarTodosPersonagens()
-      setTotalPages(info.pages)
-  
-      return todosPersonagens.map(personagem =>
-        <Card data={personagem} />
-      )
+
+    async function buildCards() {
+        const consulta = await getCharacters()
+        
+        return (
+            <>
+                { consulta.name }
+                <img src={consulta.url} alt="" />
+            </>
+        )
     }
-  
+
     useEffect(() => {
-      async function getConteudo() {
-        setConteudo(await listaPersonagens())
-      }
-      getConteudo()
-    }, [page, busca])
-  
+        async function getConteudo() {
+            setConteudo(await buildCards())
+        }
+
+        getConteudo()
+    }, [])
+
     return (
-      <div>
-        <Filter busca={busca} setBusca={setBusca} />
-        <div className='lista-principal'>
+        
+        <div className='card'>
             { conteudo }
         </div>
-        <Pagination 
-          page={page}
-          totalPages={totalPages}
-          onPageChange={setPage}
-        />
-      </div>
-    )
-  }
+    )   
+}
