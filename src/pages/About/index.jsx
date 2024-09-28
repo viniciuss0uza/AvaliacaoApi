8399e2a0795335ca34b76ed6f67b833fb08f483f
Vinícius <vinicius.m.sousa3009@gmail.com>
2024-09-27 22:01:05 -0300

    Concluído

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,8 +3,8 @@ import './styles.css'
 export default function About() {
     return (
         <div className='about-page'>
-            <h2>Venha você também descobrir o universo do Rick and Morty</h2>
-            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur atque natus amet harum nobis excepturi rerum consequatur mollitia illum dolores, tempore ratione earum, reprehenderit alias modi inventore? Nulla, dolorem quasi?</span>
+            <h2>Veja aqui trava-língua de Gato</h2>
+            <span>Gato escondido com rabo de fora ta mais escondido que rabo escondido com gato de fora.</span>
         </div>
     )
 }
